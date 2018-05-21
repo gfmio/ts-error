258bdaaebbb64b7c96a3cd227e13e932b2ef6f91
Frederique Mittelstaedt <frederique@automorph.com>
2018-05-21 08:10:37 +0100

    Remove sym link creation script

--- lib/es.d.ts
+++ lib/es.d.ts
@@ -1,1 +1,7 @@
-./types.d.ts
+/// <reference types="node" />
+export declare class ExtendableError extends Error {
+    public name: string;
+    public message: string;
+    public stack?: string;
+}
+export default ExtendableError;
